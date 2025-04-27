83a8635fba2ded9863ed582bff594d0508f59132
KamilMleczko <kmleczko@student.agh.edu.pl>
2025-04-27 14:34:45 +0200

    Introduced config for the new ESP32CAM

--- ProjektExpanded/main/main.c
+++ ProjektExpanded/main/main.c
@@ -40,7 +40,26 @@
 // oled lib
 #include "my_ssd1306.h"
 #define tag "SSD1306"
-// HC-SR04 Sensor
+// Camera
+//#include "esp_camera.h"
+
+#define PWDN_GPIO_NUM    32
+#define RESET_GPIO_NUM   -1
+#define XCLK_GPIO_NUM     0
+#define SIOD_GPIO_NUM    26
+#define SIOC_GPIO_NUM    27
+
+#define Y9_GPIO_NUM      35
+#define Y8_GPIO_NUM      34
+#define Y7_GPIO_NUM      39
+#define Y6_GPIO_NUM      36
+#define Y5_GPIO_NUM      21
+#define Y4_GPIO_NUM      19
+#define Y3_GPIO_NUM      18
+#define Y2_GPIO_NUM       5
+#define VSYNC_GPIO_NUM   25
+#define HREF_GPIO_NUM    23
+#define PCLK_GPIO_NUM    22
 
 TaskHandle_t mqtt_task_handle = NULL;
 
@@ -79,4 +98,47 @@ void init_hw_services(void) {
 void app_main(void) {
 
   ESP_LOGI(LOG_HW, "Starting application main function");
+
+  // camera_config_t config = {
+  //   .pin_pwdn = PWDN_GPIO_NUM,
+  //   .pin_reset = RESET_GPIO_NUM,
+  //   .pin_xclk = XCLK_GPIO_NUM,
+  //   .pin_sscb_sda = SIOD_GPIO_NUM,
+  //   .pin_sscb_scl = SIOC_GPIO_NUM,
+
+  //   .pin_d7 = Y9_GPIO_NUM,
+  //   .pin_d6 = Y8_GPIO_NUM,
+  //   .pin_d5 = Y7_GPIO_NUM,
+  //   .pin_d4 = Y6_GPIO_NUM,
+  //   .pin_d3 = Y5_GPIO_NUM,
+  //   .pin_d2 = Y4_GPIO_NUM,
+  //   .pin_d1 = Y3_GPIO_NUM,
+  //   .pin_d0 = Y2_GPIO_NUM,
+  //   .pin_vsync = VSYNC_GPIO_NUM,
+  //   .pin_href = HREF_GPIO_NUM,
+  //   .pin_pclk = PCLK_GPIO_NUM,
+
+  //   .xclk_freq_hz = 20000000,
+  //   .ledc_timer = LEDC_TIMER_0,
+  //   .ledc_channel = LEDC_CHANNEL_0,
+
+  //   .pixel_format = PIXFORMAT_JPEG, // For testing; can use RGB565 or GRAYSCALE
+  //   .frame_size = FRAMESIZE_QVGA,   // Small frame size for quick test
+  //   .jpeg_quality = 12,             // Lower is better quality
+  //   .fb_count = 1
+  // };
+  // esp_err_t err = esp_camera_init(&config);
+  // if (err != ESP_OK) {
+  //     ESP_LOGE(TAG, "Camera init failed with error 0x%x", err);
+  //     return;
+  // }
+
+  //   // Capture a frame
+  // camera_fb_t *fb = esp_camera_fb_get();
+  // if (!fb) {
+  //     ESP_LOGE(TAG, "Camera capture failed");
+  // } else {
+  //     ESP_LOGI(TAG, "Captured a frame! Size: %zu bytes", fb->len);
+  //     esp_camera_fb_return(fb);
+  // }
 }
